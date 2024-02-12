8bca4fd2dd34340d18ed0cf17bc3116346356af1
BokiCode <bojancodedev@gmail.com>
2024-02-12 19:02:46 +0100

    fixed input field

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     $("#toggle-color-background").click(function () {
         $("#toggle-color-circle").toggleClass("move-right");
     });
+    $(".search-field").click(function() {
+        $("#input-city-field").focus();
+    });
 });
