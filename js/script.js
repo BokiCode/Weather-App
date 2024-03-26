eb4ff88e45a0963feaa8ab71c704a9de18731a27
BokiCode <bojancodedev@gmail.com>
2024-03-26 17:14:51 +0100

    finished dark/light mode feature

--- js/script.js
+++ js/script.js
@@ -12,8 +12,10 @@ $(document).ready(function () {
         let lightModeCss = $('link[href="/css/lightmode.css"]');
         if (lightModeCss.length === 0) {
             $('link[href="css/style.css"]').after('<link rel="stylesheet" href="/css/lightmode.css" id="light-mode-stylesheet">');
+            $("#toggle-color-paragraph").text("Light Mode");
         } else {
             lightModeCss.remove();
+            $("#toggle-color-paragraph").text("Dark Mode");
         }
     });
 });
